b6eb3efe6822215fefce47421bc10228666918e4
Lisa BEKDACHE <lbekdach@e3r6p9.42.fr>
2014-06-10 18:53:13 +0200

    gui file

---
+++ gui/gui.py
@@ -0,0 +1,25 @@
+from connexions import Sock
+
+class Game:
+    s, x, y = None
+    map = {}
+    def __init__(Sock):
+        self.s = Sock
+        self.mct()
+        self.btc()
+    def msz:
+        s.send("msz")
+        message = s.read().split()
+        self.x = int(message[0])
+        self.y = int(message[1])
+        for i in range(1, self.y + 1):
+            map[i] = []
+            for j in range(1, self.x + 1):
+                map[i].append((0, 0, 0, 0, 0, 0, 0))
+    def btc:
+        s.send("mct")
+        while i < (self.x * self.y):
+            message = s.read().split()
+        
+
+        
