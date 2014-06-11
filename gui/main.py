ec7c8986fdc18d23141db2c2c7daf7d6ad4dddc7
lytchi <lytchi@tinuviel.42.fr>
2014-06-11 13:38:25 +0200

    check args

--- gui/main.py
+++ gui/main.py
@@ -10,6 +10,9 @@ def loop(s, g):
             func(message)
 
 if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        log("usage: python main.py host port")
+        exit()
     s = Sock(sys.argv[1], sys.argv[2])
     g = Game(s)
     loop(s, g)
