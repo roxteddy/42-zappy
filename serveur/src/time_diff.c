279caca337f41b3fe94f79c05b53a89c872bca29
Pierre-Alexis Ciavaldini <ciavaldp@gmail.com>
2014-06-18 19:37:40 +0200

    correction time_diff.c

--- serveur/src/time_diff.c
+++ serveur/src/time_diff.c
@@ -6,7 +6,7 @@
 /*   By: pciavald <pciavald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/16 19:48:32 by pciavald          #+#    #+#             */
-/*   Updated: 2014/06/18 18:28:30 by pciavald         ###   ########.fr       */
+/*   Updated: 2014/06/18 19:33:49 by pciavald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,14 @@ typedef struct timeval		t_timeval;
 
 int		time_diff(t_timeval *t1, t_timeval *t2)
 {
-	if (t2->tv_usec > t1->tv_usec)
+	t_timeval	t3;
+
+	t3.tv_sec = t2->tv_sec - t1->tv_sec;
+	t3.tv_usec = t2->tv_usec - t1->tv_usec;
+	if (t3.tv_sec > 0 || (t3.tv_sec == 0 && t3.tv_usec > 0))
 		return (1);
-	if (t2->tv_usec == t1->tv_usec)
+	if (t3.tv_sec == 0 && t3.tv_usec == 0)
 		return (0);
-	if (t2->tv_usec < t1->tv_usec)
+	if (t3.tv_sec < 0 || (t3.tv_sec == 0 && t3.tv_usec < 0))
 		return (-1);
 }
