d562c871b57aad2593ac4f8655bbef20f1ba08e0
Pierre-Alexis Ciavaldini <ciavaldp@gmail.com>
2014-06-21 18:32:43 +0200

    look

--- serveur/src/cmd_look.c
+++ serveur/src/cmd_look.c
@@ -6,47 +6,48 @@
 /*   By: pciavald <pciavald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/20 19:49:13 by pciavald          #+#    #+#             */
-/*   Updated: 2014/06/20 20:17:24 by pciavald         ###   ########.fr       */
+/*   Updated: 2014/06/20 20:33:55 by pciavald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 #include <string.h>
 
-static t_square		*see(t_data *data, int *seen, int level)
+static t_square		*see(t_data *data, int seen, int *x, int *y)
 {
-	t_square	*square;
-	int			x;
-	int			y;
+	t_square		*square;
+	int				level;
 
-	square = get_square(data, x, y);
+	level = data->fds[cs].player.level;
+	square = get_square(data, *x, *y);
 	return (square);
 }
 
-void	cmd_look(t_data *data, int cs, char **cmd)
+void				cmd_look(t_data *data, int cs, char **cmd)
 {
-	t_square	**map;
-	t_square	*square;
-	char		buf[BUF_SIZE];
-	int			i;
-	int			level;
+	t_square		*square;
+	char			buf[BUF_SIZE];
+	int				x;
+	int				y;
+	int				i;
 
 	(void)cmd;
-	map = data->map;
-	level = data->fds[cs].player.level;
 	memset(buf, '\0', BUF_SIZE);
+	x = data->fds[cs].player.x;
+	y = data->fds[cs].player.y;
 	i = 0;
-	while ((square = see(data, &i, level)) != NULL)
+	while ((square = see(data, i, &x, &y)) != NULL)
 	{
-	dprintf(cs, "bct %d %d %d %d %d %d %d %d %d\n",
-			square->x,
-			square->y,
-			square->food,
-			square->linemate,
-			square->deraumere,
-			square->sibur,
-			square->mendiane,
-			square->phiras,
-			square->thystame);
+		dprintf(cs, "bct %d %d %d %d %d %d %d %d %d\n",
+				square->x,
+				square->y,
+				square->food,
+				square->linemate,
+				square->deraumere,
+				square->sibur,
+				square->mendiane,
+				square->phiras,
+				square->thystame);
+		i++;
 	}
 }
