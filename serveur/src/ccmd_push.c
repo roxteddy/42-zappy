f25272c3464afdcb7736ae04aef1c847a8e64488
Rox Teddy <RoxTeddy@irox.home>
2014-06-23 01:27:22 +0200

    server: implemented ccmd_push()

--- serveur/src/ccmd_push.c
+++ serveur/src/ccmd_push.c
@@ -6,16 +6,75 @@
 /*   By: mfebvay <mfebvay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/20 21:35:35 by mfebvay           #+#    #+#             */
-/*   Updated: 2014/06/22 23:01:23 by pciavald         ###   ########.fr       */
+/*   Updated: 2014/06/23 01:26:31 by mfebvay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
+#include <stdlib.h>
+#include <sys/time.h>
 
-void	ccmd_push(t_data *data, int cs, char **cmd, t_timeval **t)
+static void	timer_init(t_data *data, t_timeval **timer, t_player *player)
 {
-	(void)data;
-	(void)cs;
+	t_timeval	now;
+
+	gettimeofday(&now, NULL);
+	*timer = (t_timeval*)malloc(sizeof(t_timeval));
+	**timer = time_add(data, &now, DROP_T);
+	gui_broadcast(data, gui_pex, player);
+}
+
+static int	push(t_data *data, t_player *pusher, t_player *target)
+{
+	if (pusher->o == N)
+		target->y = mod(target->y - 1,data->y);
+    else if (pusher->o == S)
+		target->y = mod(target->y + 1, data->y);
+    else if (pusher->o == W)
+		target->x = mod(target->x - 1, data->x);
+    else if (pusher->o == E)
+		target->x = mod(target->x + 1, data->x);
+	dprintf(target->cs, "deplacement %d\n", target->o);
+	gui_broadcast(data, gui_ppo, target);
+	return (1);
+}
+
+static int	find(t_data *data, t_player *player)
+{
+	t_tlist		*team;
+	t_plist		*list;
+	int			count;
+
+	count = 0;
+	team = data->teams;
+	while (team)
+	{
+		list = team->list;
+		while (list)
+		{
+			if (list->player != player
+				&& list->player->x == player->x && list->player->y == player->y)
+				count += push(data, player, list->player);
+			list = list->next;
+		}
+		team = team->next;
+	}
+	return (count);
+}
+
+void		ccmd_push(t_data *data, int cs, char **cmd, t_timeval **t)
+{
+	t_player	*player;
+
 	(void)cmd;
-	(void)t;
+	player = &data->fds[cs].player;
+	if (!(*t))
+		timer_init(data, t, player);
+	else
+	{
+		if (find(data, player))
+			dprintf(cs, "ok\n");
+		else
+			dprintf(cs, "ko\n");
+	}
 }
