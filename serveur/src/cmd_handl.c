f595a28531159bd87ecc051848f4c2efbef252ce
Rox Teddy <RoxTeddy@irox.home>
2014-06-26 15:04:07 +0200

    server:gui  cmd handler

--- serveur/src/cmd_handl.c
+++ serveur/src/cmd_handl.c
@@ -6,7 +6,7 @@
 /*   By: mfebvay <mfebvay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/14 10:22:52 by mfebvay           #+#    #+#             */
-/*   Updated: 2014/06/23 11:35:42 by pciavald         ###   ########.fr       */
+/*   Updated: 2014/06/26 12:01:07 by mfebvay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,21 +33,32 @@ static void		player_handl(t_data *data, char **cmd, int cs)
 		dprintf(cs, "command unknown\n");
 }
 
+static void		gui_handl(t_data *data, char **cmd, int cs)
+{
+	int		i;
+
+	i = -1;
+	while (++i < GCMD_NB)
+	{
+		if (!strcmp(cmd[0], data->gcmds[i].cmd))
+		{
+			data->gcmds[i].fct(data, cs, cmd);
+			break ;
+		}
+	}
+	if (i == GCMD_NB)
+		gui_suc(NULL, cs);
+}
+
 static void		client_handl(t_data *data, char *cmd, int cs)
 {
 	if (!strcmp(cmd, "GRAPHIC"))
 	{
-		//
-		printf("New GUI connected\n");
 		data->fds[cs].type = FD_GUI;
 		gui_init(data, cs);
 	}
 	else
-	{
-		//
-		printf("New player connected\n");
 		team_join(data, cmd, cs);
-	}
 }
 
 void			cmd_handl(t_data *data, char *cmd, int cs)
@@ -62,8 +73,8 @@ void			cmd_handl(t_data *data, char *cmd, int cs)
 			client_handl(data, cmd, cs);
 		else if (data->fds[cs].type == FD_PLAYER)
 			player_handl(data, cmd_split, cs);
-		//	else if (data->fds[cs].type == FD_GUI)
-		//		gui_handl(data, cmd_split, cs);
+		else if (data->fds[cs].type == FD_GUI)
+			gui_handl(data, cmd_split, cs);
 		free_split(cmd_split);
 	}
 }
